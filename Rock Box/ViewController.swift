16cf8b83063cf7cdad7d87ad76f83faad25e3a45
Steve Sparks <sparks@bignerdranch.com>
2014-07-16 13:48:24 -0400

    adds core motion: boxes respond to device orientation

--- Rock Box/ViewController.swift
+++ Rock Box/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
     // Most conservative guess. We'll set them later.
@@ -15,6 +16,10 @@ class ViewController: UIViewController {
     let boxSize : CGFloat = 30.0
     var boxes : Array<UIView> = []
 
+    // For getting device motion updates
+    let motionQueue = NSOperationQueue()
+    let motionManager = CMMotionManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         maxX = super.view.bounds.size.width - boxSize;
@@ -22,6 +27,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         createAnimatorStuff()
         generateBoxes()
+    }
+
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        NSLog("Starting gravity")
+        motionManager.startDeviceMotionUpdatesToQueue(motionQueue, withHandler: gravityUpdated)
+    }
+
+    override func viewDidDisappear(animated: Bool)  {
+        super.viewDidDisappear(animated)
+        NSLog("Stopping gravity")
+        motionManager.stopDeviceMotionUpdates()
     }
 
     override func didReceiveMemoryWarning() {
@@ -98,6 +115,36 @@ class ViewController: UIViewController {
         collider.addItem(box)
     }
 
+    //----------------- Core Motion
+    func gravityUpdated(motion: CMDeviceMotion!, error: NSError!) {
+        let grav : CMAcceleration = motion.gravity;
 
+        let x = CGFloat(grav.x);
+        let y = CGFloat(grav.y);
+        var p = CGPointMake(x,y)
+
+        if error {
+            NSLog("\(error)")
+        }
+
+        // Have to correct for orientation.
+        var orientation = UIApplication.sharedApplication().statusBarOrientation;
+
+        if(orientation == UIInterfaceOrientation.LandscapeLeft) {
+            var t = p.x
+            p.x = 0 - p.y
+            p.y = t
+        } else if (orientation == UIInterfaceOrientation.LandscapeRight) {
+            var t = p.x
+            p.x = p.y
+            p.y = 0 - t
+        } else if (orientation == UIInterfaceOrientation.PortraitUpsideDown) {
+            p.x *= -1
+            p.y *= -1
+        }
+        
+        var v = CGVectorMake(p.x, 0 - p.y);
+        gravity.gravityDirection = v;
+    }
 }
 
